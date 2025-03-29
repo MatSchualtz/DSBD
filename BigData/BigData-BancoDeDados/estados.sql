fa7b13d6e9530b823ba25493035a3652a87db5fd
Mateus Schipanski Schualtz <msschualtz@inf.ufpr.br>
2025-03-28 21:12:11 -0300

    Aula 02 - BigData

--- BigData/BigData-BancoDeDados/estados.sql
+++ BigData/BigData-BancoDeDados/estados.sql
@@ -1,27 +1,35 @@
-INSERT INTO estados (uf, nome) VALUES ('AC', 'Acre');
-INSERT INTO estados (uf, nome) VALUES ('AL', 'Alagoas');
-INSERT INTO estados (uf, nome) VALUES ('AP', 'Amapá');
-INSERT INTO estados (uf, nome) VALUES ('AM', 'Amazonas');
-INSERT INTO estados (uf, nome) VALUES ('BA', 'Bahia');
-INSERT INTO estados (uf, nome) VALUES ('CE', 'Ceará');
-INSERT INTO estados (uf, nome) VALUES ('DF', 'Distrito Federal');
-INSERT INTO estados (uf, nome) VALUES ('ES', 'Espírito Santo');
-INSERT INTO estados (uf, nome) VALUES ('GO', 'Goiás');
-INSERT INTO estados (uf, nome) VALUES ('MA', 'Maranhão');
-INSERT INTO estados (uf, nome) VALUES ('MT', 'Mato Grosso');
-INSERT INTO estados (uf, nome) VALUES ('MS', 'Mato Grosso do Sul');
-INSERT INTO estados (uf, nome) VALUES ('MG', 'Minas Gerais');
-INSERT INTO estados (uf, nome) VALUES ('PA', 'Pará');
-INSERT INTO estados (uf, nome) VALUES ('PB', 'Paraíba');
-INSERT INTO estados (uf, nome) VALUES ('PR', 'Paraná');
-INSERT INTO estados (uf, nome) VALUES ('PE', 'Pernambuco');
-INSERT INTO estados (uf, nome) VALUES ('PI', 'Piauí');
-INSERT INTO estados (uf, nome) VALUES ('RJ', 'Rio de Janeiro');
-INSERT INTO estados (uf, nome) VALUES ('RN', 'Rio Grande do Norte');
-INSERT INTO estados (uf, nome) VALUES ('RS', 'Rio Grande do Sul');
-INSERT INTO estados (uf, nome) VALUES ('RO', 'Rondônia');
-INSERT INTO estados (uf, nome) VALUES ('RR', 'Roraima');
-INSERT INTO estados (uf, nome) VALUES ('SC', 'Santa Catarina');
-INSERT INTO estados (uf, nome) VALUES ('SP', 'São Paulo');
-INSERT INTO estados (uf, nome) VALUES ('SE', 'Sergipe');
-INSERT INTO estados (uf, nome) VALUES ('TO', 'Tocantins');
+DROP TABLE IF EXISTS municipio;
+DROP TABLE IF EXISTS estado;
+
+CREATE TABLE estado(
+    uf TEXT PRIMARY KEY,
+    nome TEXT NOT NULL UNIQUE
+);
+
+INSERT INTO estado (uf, nome) VALUES ('AC', 'Acre');
+INSERT INTO estado (uf, nome) VALUES ('AL', 'Alagoas');
+INSERT INTO estado (uf, nome) VALUES ('AP', 'Amapá');
+INSERT INTO estado (uf, nome) VALUES ('AM', 'Amazonas');
+INSERT INTO estado (uf, nome) VALUES ('BA', 'Bahia');
+INSERT INTO estado (uf, nome) VALUES ('CE', 'Ceará');
+INSERT INTO estado (uf, nome) VALUES ('DF', 'Distrito Federal');
+INSERT INTO estado (uf, nome) VALUES ('ES', 'Espírito Santo');
+INSERT INTO estado (uf, nome) VALUES ('GO', 'Goiás');
+INSERT INTO estado (uf, nome) VALUES ('MA', 'Maranhão');
+INSERT INTO estado (uf, nome) VALUES ('MT', 'Mato Grosso');
+INSERT INTO estado (uf, nome) VALUES ('MS', 'Mato Grosso do Sul');
+INSERT INTO estado (uf, nome) VALUES ('MG', 'Minas Gerais');
+INSERT INTO estado (uf, nome) VALUES ('PA', 'Pará');
+INSERT INTO estado (uf, nome) VALUES ('PB', 'Paraíba');
+INSERT INTO estado (uf, nome) VALUES ('PR', 'Paraná');
+INSERT INTO estado (uf, nome) VALUES ('PE', 'Pernambuco');
+INSERT INTO estado (uf, nome) VALUES ('PI', 'Piauí');
+INSERT INTO estado (uf, nome) VALUES ('RJ', 'Rio de Janeiro');
+INSERT INTO estado (uf, nome) VALUES ('RN', 'Rio Grande do Norte');
+INSERT INTO estado (uf, nome) VALUES ('RS', 'Rio Grande do Sul');
+INSERT INTO estado (uf, nome) VALUES ('RO', 'Rondônia');
+INSERT INTO estado (uf, nome) VALUES ('RR', 'Roraima');
+INSERT INTO estado (uf, nome) VALUES ('SC', 'Santa Catarina');
+INSERT INTO estado (uf, nome) VALUES ('SP', 'São Paulo');
+INSERT INTO estado (uf, nome) VALUES ('SE', 'Sergipe');
+INSERT INTO estado (uf, nome) VALUES ('TO', 'Tocantins');
